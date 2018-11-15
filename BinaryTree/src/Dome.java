94c578f1df77503e14d32435b5f19496bb5d9dba
LiYuanShu <1773475831@qq.com>
2018-11-15 14:44:57 +0800

    yes

---
+++ BinaryTree/src/Dome.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+public class Dome 
+{
+
+	 public static void main(String[] args)
+	 {
+		 Window win=new Window("¶þ²æÊ÷");
+	 }
+	
+}
---
+++ BinaryTree/src/Node.java
@@ -0,0 +1,14 @@
+
+class Node
+ {  
+	 Node left=null;
+	  Node right=null;
+	 char value;
+	 public Node(char split) 
+	{
+	  this.value = split;
+	  left=null;
+	  right=null;
+	}
+  }  
+
